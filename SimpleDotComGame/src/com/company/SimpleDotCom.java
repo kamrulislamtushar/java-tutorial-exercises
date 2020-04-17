64f21063c035404a63d71c426c25f3f914e1298c
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2020-04-17 12:34:44 +0600

    Dot com simple game

---
+++ SimpleDotComGame/src/com/company/SimpleDotCom.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class SimpleDotCom {
+
+    int[] locationCells;
+    int numOfHits = 0;
+    public void setLocationCells(int[] locations) {
+
+        locationCells = locations;
+    }
+    public String checkYourself(String guessedResult) {
+        int guess = Integer.parseInt(guessedResult);
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
