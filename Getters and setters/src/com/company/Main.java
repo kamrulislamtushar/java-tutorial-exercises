8d3bdb5e27cf90a2a03dd2d52118cddd7b997909
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2020-04-19 19:11:00 +0600

    Getters and setters

---
+++ Getters and setters/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Unknown u = new Unknown();
+
+        u.setName("Kamrul Islam Tushar");
+        System.out.print(u.getName());
+
+        u.setAge(26);
+        System.out.print(u.getAge());
+    }
+}
