11d407474742ae23edf56254ba2410384a9b5836
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2020-04-19 23:39:14 +0600

    Polymorphism

---
+++ Polymorphism/src/com/company/Plant.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Plant {
+    public void grow() {
+        System.out.print("Plant is growing");
+    }
+}
---
+++ Polymorphism/src/com/company/Tree.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Tree extends Plant {
+    public void grow() {
+        System.out.print("Tree is growing");
+    }
+}
